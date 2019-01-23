86b50174765a559e54299ec35a4ec800ad36878d
hakim <hakimnoralahyadi@gmail.com>
2019-01-23 18:03:46 -0500

    added accuracy score

--- scikitClassifierModels.py
+++ scikitClassifierModels.py
@@ -1,53 +1,47 @@
 from sklearn import tree
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import RadiusNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-clf = tree.DecisionTreeClassifier()
-clf2 = KNeighborsClassifier()
-clf3 = RadiusNeighborsClassifier(radius=1000.0)
-clf4 = SVC(kernel="linear", C=0.025,random_state=101)
-clf5 = RandomForestClassifier(n_estimators= 10)
-#ValueError: No neighbors found for test samples [0], you can try using larger radius,
-# give a label for outliers, or consider removing them from your dataset.
-
-
-
-# CHALLENGE - create 3 more classifiers...
-# 1 DecisionTreeClassifier
-# 2 KNeighborsClassifier
-# 3 RadiusNeighborsClassifier
-#4 SVC
-
-# [height, weight, shoe_size]
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
-     [190, 90, 47], [175, 64, 39],
+# Data and labels
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
-     'female', 'male', 'male']
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
 
+# Classifiers
+# using the default values for all the hyperparameters
+clf_tree = tree.DecisionTreeClassifier()
+clf_svm = SVC()
+clf_perceptron = Perceptron()
+clf_KNN = KNeighborsClassifier()
 
-# CHALLENGE - ...and train them on our data
-clf = clf.fit(X, Y)
-clf2 = clf2.fit(X, Y)
-clf3 = clf3.fit(X,Y)
-clf4 = clf4.fit(X,Y)
-clf5 = clf5.fit(X,Y)
+# Training the models
+clf_tree.fit(X, Y)
+clf_svm.fit(X, Y)
+clf_perceptron.fit(X, Y)
+clf_KNN.fit(X, Y)
 
+# Testing using the same data
+pred_tree = clf_tree.predict(X)
+acc_tree = accuracy_score(Y, pred_tree) * 100
+print('Accuracy for DecisionTree: {}'.format(acc_tree))
 
-prediction = clf.predict([[190, 70, 43]])
-prediction2 = clf2.predict([[190, 70, 43]])
-prediction3 = clf3.predict([[190, 70, 43]])
-prediction4 = clf4.predict([[190, 70, 43]])
-prediction5 = clf5.predict([[190, 70, 43]])
+pred_svm = clf_svm.predict(X)
+acc_svm = accuracy_score(Y, pred_svm) * 100
+print('Accuracy for SVM: {}'.format(acc_svm))
 
+pred_per = clf_perceptron.predict(X)
+acc_per = accuracy_score(Y, pred_per) * 100
+print('Accuracy for perceptron: {}'.format(acc_per))
 
-# CHALLENGE compare their reusults and print the best one!
+pred_KNN = clf_KNN.predict(X)
+acc_KNN = accuracy_score(Y, pred_KNN) * 100
+print('Accuracy for KNN: {}'.format(acc_KNN))
 
-print(prediction)
-print(prediction2)
-print(prediction3)
-print(prediction4)
-print(prediction5)
+# The best classifier from svm, per, KNN
+index = np.argmax([acc_svm, acc_per, acc_KNN])
+classifiers = {0: 'SVM', 1: 'Perceptron', 2: 'KNN'}
+print('Best gender classifier is {}'.format(classifiers[index]))
